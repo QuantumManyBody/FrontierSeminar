7f1936d19b6ac476ffdb188ea9f3fa4cdb178d1f
Kangbo <li95@llnl.gov>
2025-08-15 15:14:56 -0400

    speaker instruction

--- render_math.js
+++ render_math.js
@@ -1,9 +1,13 @@
 const latexInput = document.getElementById('latexInput');
 const latexPreview = document.getElementById('latexPreview');
+const characterCount = document.getElementById('characterCount');
+
 latexPreview.innerHTML = latexInput.value; 
 renderMathInElement(document.body)
+characterCount.textContent = `count: ${latexInput.value.length}/1000`
 
 latexInput.addEventListener('input', () => {
     latexPreview.innerHTML = latexInput.value; 
-renderMathInElement(document.body)
+    renderMathInElement(document.body)
+    characterCount.textContent = `count: ${latexInput.value.length}/1000`; 
 });
