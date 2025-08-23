493ca0d5ed0c6ba7a3d5baa6a80f6ba74c6aa091
Kangbo <li95@llnl.gov>
2025-08-23 19:21:22 -0400

    update index.html

--- flyer_maker/flyer_maker.js
+++ flyer_maker/flyer_maker.js
@@ -1,8 +1,8 @@
 // Usage: node poster_maker.js <speaker_id> <food>
-
 const jsdom = require("jsdom");
 const args = process.argv.slice(2);
 const speaker_id = args[0]
+// const speaker_id = 'jmandell'
 
 const fs = require("fs");
 
@@ -11,14 +11,15 @@ const frontier_webpage = fs.readFileSync("../index.html", "utf8");
 
 const dom = new jsdom.JSDOM(frontier_webpage);
 
-speaker_row = dom.window.document.getElementById(speaker_id).querySelectorAll("*");
+speaker_row = dom.window.document.getElementById(speaker_id)
 
-date = speaker_row[0].innerHTML
-title = speaker_row[3].innerHTML
-abstract = speaker_row[4].innerHTML
-bio = speaker_row[5].innerHTML
-speaker_name = speaker_row[7].innerHTML
-affiliation = speaker_row[8].innerHTML.replace("/", "")
+date = speaker_row.getElementsByClassName("speaker-date")[0].innerHTML
+title = speaker_row.getElementsByClassName("speaker-title")[0].innerHTML
+abstract = speaker_row.getElementsByClassName("speaker-abstract")[0].innerHTML
+bio = speaker_row.getElementsByClassName("speaker-bio")[0].innerHTML
+speaker_name = speaker_row.getElementsByClassName("speaker-name")[0].innerHTML
+affiliation = speaker_row.getElementsByClassName("affiliation")[0].innerHTML
+food = speaker_row.getElementsByClassName("speaker-food")[0].innerHTML
 flyer = fs.readFileSync("./template.html", "utf8");
 
 const flyer_dom = new jsdom.JSDOM(flyer);
@@ -29,7 +30,11 @@ flyer_dom.window.document.getElementById("title").innerHTML = title
 flyer_dom.window.document.getElementById("speaker-name").innerHTML = speaker_name
 flyer_dom.window.document.getElementById("abstract").innerHTML = abstract
 flyer_dom.window.document.getElementById("affiliation").innerHTML = affiliation
-flyer_dom.window.document.getElementById("food").innerHTML = args[1] 
+if (food !== undefined) {
+    flyer_dom.window.document.getElementById("food").innerHTML = food
+}
+
+
 
 if (bio.trim().length == 0) {
      flyer_dom.window.document.getElementById("bio-block").remove();   
